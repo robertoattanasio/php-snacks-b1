07f65b00ae15f940b907ae597034bb7967f9270d
Roberto <r.attanasio90@gmail.com>
2021-04-23 16:09:52 +0200

    Corretta indentazione Snack 1

--- snack_1/index.php
+++ snack_1/index.php
@@ -7,53 +7,70 @@
     <title>PHP Snack 1</title>
 </head>
 <body>
+    <h2>Utilizzo Ciclo For</h2>
     <?php 
-
     $listaPartite = [
         [
-            'SquadraUnoNome' => 'Milan',
-            'SquadraUnoRisultato' =>  0,
-            'SquadraDueNome' => 'Inter',
-            'SquadraDueRisultato' =>  2,
+        'SquadraUnoNome' => 'Milan',
+        'SquadraUnoRisultato' =>  0,
+        'SquadraDueNome' => 'Inter',
+        'SquadraDueRisultato' =>  2,
         ],
         [
-            'SquadraUnoNome' => 'Juventus',
-            'SquadraUnoRisultato' =>  2,
-            'SquadraDueNome' => 'Napoli',
-            'SquadraDueRisultato' =>  1,
+        'SquadraUnoNome' => 'Juventus',
+        'SquadraUnoRisultato' =>  2,
+        'SquadraDueNome' => 'Napoli',
+        'SquadraDueRisultato' =>  1,
         ],
         [
-            'SquadraUnoNome' => 'Cagliari',
-            'SquadraUnoRisultato' =>  4,
-            'SquadraDueNome' => 'Sassuolo',
-            'SquadraDueRisultato' =>  2,
+        'SquadraUnoNome' => 'Cagliari',
+        'SquadraUnoRisultato' =>  4,
+        'SquadraDueNome' => 'Sassuolo',
+        'SquadraDueRisultato' =>  2,
         ],
         [
-            'SquadraUnoNome' => 'Bologna',
-            'SquadraUnoRisultato' =>  1,
-            'SquadraDueNome' => 'Atalanta',
-            'SquadraDueRisultato' =>  0,
+        'SquadraUnoNome' => 'Bologna',
+        'SquadraUnoRisultato' =>  1,
+        'SquadraDueNome' => 'Atalanta',
+        'SquadraDueRisultato' =>  0,
         ],
         [
-            'SquadraUnoNome' => 'Lecce',
-            'SquadraUnoRisultato' =>  0,
-            'SquadraDueNome' => 'Sampdoria',
-            'SquadraDueRisultato' =>  0,
+        'SquadraUnoNome' => 'Lecce',
+        'SquadraUnoRisultato' =>  0,
+        'SquadraDueNome' => 'Sampdoria',
+        'SquadraDueRisultato' =>  0,
         ],
-        ];
+    ];
 
+// UTILIZZO CICLO FOR
 
+    for($i = 0; $i < count($listaPartite); ++$i) {
+        echo 
+            $listaPartite[$i]['SquadraUnoNome']. 
+            ' - ' .
+            $listaPartite[$i]['SquadraDueNome'].
+            ' | '.
+            $listaPartite[$i]['SquadraUnoRisultato'].
+            ' - '.
+            $listaPartite[$i]['SquadraDueRisultato']. '<br><br>';
+    }
 
-for($i = 0; $i < count($listaPartite); ++$i) {
-    echo 
-        $listaPartite[$i]['SquadraUnoNome']. 
-        ' - ' .
-        $listaPartite[$i]['SquadraDueNome'].
-        ' | '.
-        $listaPartite[$i]['SquadraUnoRisultato'].
-        ' - '.
-        $listaPartite[$i]['SquadraDueRisultato']. '<br><br>';
- }
+    ?>
+
+    <h2>Utilizzo Ciclo ForEach</h2>
+
+    <?php
+    // UTILIZZO CICLO FOREACH
+        foreach ($listaPartite as $partita) {
+            echo 
+            $partita['SquadraUnoNome']. 
+            ' - ' .
+            $partita['SquadraDueNome'].
+            ' | '.
+            $partita['SquadraUnoRisultato'].
+            ' - '.
+            $partita['SquadraDueRisultato']. '<br><br>';
+        }
     ?>
 </body>
 </html>
