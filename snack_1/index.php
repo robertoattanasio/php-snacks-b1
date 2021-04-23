caa797082bdf137b50cfd16b1dd920f0c5e593b0
Roberto <r.attanasio90@gmail.com>
2021-04-23 15:59:31 +0200

    Snack 1 completato

---
+++ snack_1/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Snack 1</title>
+</head>
+<body>
+    <?php 
+
+    $listaPartite = [
+        [
+            'SquadraUnoNome' => 'Milan',
+            'SquadraUnoRisultato' =>  0,
+            'SquadraDueNome' => 'Inter',
+            'SquadraDueRisultato' =>  2,
+        ],
+        [
+            'SquadraUnoNome' => 'Juventus',
+            'SquadraUnoRisultato' =>  2,
+            'SquadraDueNome' => 'Napoli',
+            'SquadraDueRisultato' =>  1,
+        ],
+        [
+            'SquadraUnoNome' => 'Cagliari',
+            'SquadraUnoRisultato' =>  4,
+            'SquadraDueNome' => 'Sassuolo',
+            'SquadraDueRisultato' =>  2,
+        ],
+        [
+            'SquadraUnoNome' => 'Bologna',
+            'SquadraUnoRisultato' =>  1,
+            'SquadraDueNome' => 'Atalanta',
+            'SquadraDueRisultato' =>  0,
+        ],
+        [
+            'SquadraUnoNome' => 'Lecce',
+            'SquadraUnoRisultato' =>  0,
+            'SquadraDueNome' => 'Sampdoria',
+            'SquadraDueRisultato' =>  0,
+        ],
+        ];
+
+
+
+for($i = 0; $i < count($listaPartite); ++$i) {
+    echo 
+        'La partita tra ' . 
+        $listaPartite[$i]['SquadraUnoNome']. 
+        ' e ' .
+        $listaPartite[$i]['SquadraDueNome'].
+        ' è finita con il risultato di '.
+        $listaPartite[$i]['SquadraUnoRisultato'].
+        ' | '.
+        $listaPartite[$i]['SquadraDueRisultato']. '<br><br>';
+ }
+    ?>
+</body>
+</html>
