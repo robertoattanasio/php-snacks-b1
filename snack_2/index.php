c23520c543eefbbca37ff0f7c9b20d3bc00a4754
Roberto <r.attanasio90@gmail.com>
2021-04-23 16:44:37 +0200

    Snack 2 completato

--- snack_2/index.php
+++ snack_2/index.php
@@ -6,7 +6,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Snack 2</title>
 </head>
+
 <body>
-    
+    <?php
+    $name = $_GET['name'];
+    $email = $_GET['email'];
+    $age = $_GET['age'];
+
+    // CHECK DEL NOME
+    if ((strlen($name) > 2) && !is_nan($name)) {
+
+        // CHECK DELL'EMAIL
+        if ((strpos($email, '.') == true) && (strpos($email, '@') == true)) {
+
+            // CHECK DELL'ETA
+            if (is_numeric($age)) {
+                echo 'ACCESSO RIUSCITO';
+            } else {
+                echo 'ACCESSO NEGATO. Hai inserito un\'età non valida.';
+            }
+
+        } else {
+            echo 'ACCESSO NEGATO. Hai inserito una mail non valida.';
+        }
+
+    } else {
+        echo 'ACCESSO NEGATO. Hai inserito un nome non valido.';
+    }
+  
+    ?>
 </body>
 </html>
